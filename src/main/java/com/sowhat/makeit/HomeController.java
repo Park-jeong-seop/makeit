61418dacf5a7b20086eef3a31b16ac7f34694661
Park-Jeong-seop <twinpapa2003@gmail.com>
2017-04-05 14:27:54 +0900

    no message

---
+++ src/main/java/com/sowhat/makeit/HomeController.java
@@ -0,0 +1,161 @@
+package com.sowhat.makeit;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.util.*;
+/*
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+*/
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index";
+	}
+	
+	@RequestMapping(value = "/KakaoLogin", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public String KakaoLogin(@RequestParam("code") String code , HttpServletRequest request, HttpServletResponse httpservlet)
+	{
+		System.out.println("code : "+code);
+		
+		JsonNode jsonToken = getAccessToken_Kakao(code);
+		System.out.println("JSON 반환 : " + jsonToken.get("access_token"));
+		return "KakaoLogin";
+	}
+	
+	public static JsonNode getAccessToken_Kakao(String code){ 
+	    final String RequestUrl = "https://kauth.kakao.com/oauth/token";
+
+	    final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+	    postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
+	    postParams.add(new BasicNameValuePair("client_id", "7e0540b264fec8a954659770843a3067"));    // REST API KEY
+	    postParams.add(new BasicNameValuePair("redirect_uri", "http://localhost:12219/makeit/KakaoLogin"));    // 리다이렉트 URI
+	    postParams.add(new BasicNameValuePair("code", code));    // 로그인 과정중 얻은 code 값
+
+	    final HttpClient client = HttpClientBuilder.create().build();
+	    final HttpPost post = new HttpPost(RequestUrl);
+	    JsonNode returnNode = null;
+	
+	    try {
+	      post.setEntity(new UrlEncodedFormEntity(postParams));
+	      final HttpResponse response = client.execute(post);
+	      final int responseCode = response.getStatusLine().getStatusCode();
+
+	      System.out.println("\nSending 'POST' request to URL : " + RequestUrl);
+	      System.out.println("Post parameters : " + postParams);
+	      System.out.println("Response Code : " + responseCode);
+	     
+	      //JSON 형태 반환값 처리
+	      ObjectMapper mapper = new ObjectMapper();
+	      returnNode = mapper.readTree(response.getEntity().getContent());
+
+	    } catch (UnsupportedEncodingException e) {
+	      e.printStackTrace();
+	    } catch (ClientProtocolException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    } finally {
+	        // clear resources
+	    }
+	    
+	    return returnNode;
+	}
+	
+	@RequestMapping(value = "/FacebookLogin", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public String FacebookLogin(@RequestParam("code") String code , HttpServletRequest request, HttpServletResponse httpservlet)
+	{
+		System.out.println("code : "+code);
+		
+		JsonNode jsonToken = getAccessToken_Facebook(code);
+		System.out.println("JSON 반환 : " + jsonToken.get("access_token"));
+		return "FacebookLogin";
+	}
+	
+	public static JsonNode getAccessToken_Facebook(String code){ 
+	    final String RequestUrl = "https://graph.facebook.com/v2.8/oauth/access_token";
+
+	    final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+	    postParams.add(new BasicNameValuePair("client_id", "1227350160653331"));
+	    postParams.add(new BasicNameValuePair("redirect_uri", "http://localhost:12219/makeit/FacebookLogin"));    // REST API KEY
+	    postParams.add(new BasicNameValuePair("client_secret", "7534e26b7b3c095610bd788ab419db93"));    // 리다이렉트 URI
+	    postParams.add(new BasicNameValuePair("code", code));    // 로그인 과정중 얻은 code 값
+
+	    final HttpClient client = HttpClientBuilder.create().build();
+	    final HttpPost post = new HttpPost(RequestUrl);
+	    JsonNode returnNode = null;
+	
+	    try {
+	      post.setEntity(new UrlEncodedFormEntity(postParams));
+	      final HttpResponse response = client.execute(post);
+	      final int responseCode = response.getStatusLine().getStatusCode();
+
+	      System.out.println("\nSending 'POST' request to URL : " + RequestUrl);
+	      System.out.println("Post parameters : " + postParams);
+	      System.out.println("Response Code : " + responseCode);
+	     
+	      //JSON 형태 반환값 처리
+	      ObjectMapper mapper = new ObjectMapper();
+	      returnNode = mapper.readTree(response.getEntity().getContent());
+
+	    } catch (UnsupportedEncodingException e) {
+	      e.printStackTrace();
+	    } catch (ClientProtocolException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    } finally {
+	        // clear resources
+	    }
+	    
+	    return returnNode;
+	}
+
+	
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="<c:url value="/resources/css/default.css" />" rel="stylesheet" type="text/css" />
+
+<script language="javascript" type="text/javascript">
+	function openKakao(){  
+	    window.open("https://kauth.kakao.com/oauth/authorize?client_id=7e0540b264fec8a954659770843a3067&redirect_uri=http://localhost:12219/makeit/KakaoLogin&response_type=code", "카카오 로그인","width=200, height=400, toolbar=no, menubar=no, scrollbars=no, resizable=yes" );  
+	}
+	
+	function openFacebook(){  
+	    window.open("https://www.facebook.com/v2.8/dialog/oauth?client_id=1227350160653331&redirect_uri=http://localhost:12219/makeit/FacebookLogin&response_type=code", "페이스북 로그인","width=200, height=400, toolbar=no, menubar=no, scrollbars=no, resizable=yes" );  
+	}
+</script>
+
+</head>
+<body>
+<div id="wrap">
+	<div class="main_container">
+		<div class="slogun">
+			<dl>
+				<dd class="logo_name">make it</dd>
+				<dd class="logo_help">같이 만드는 가치</dd>
+			</dl>
+		</div>
+		<div class="btn_list">
+			<a href="" onclick="openFacebook()"><p class="btn_facebook"><b>페이스북으로 시작하기</b></p></a>
+			<a href="" onclick="openKakao()"><p class="btn_kakao"><b>카카오톡으로 시작하기</b></p></a>
+		</div>
+	</div>
+	
+	<div class="main_bottom">
+		<div class="main_bottom_contatiner">
+			<ul>
+				<li>
+					<a href="/makeit/" ><p class="main_bottom_tag1"><b>ABOUT MAKE IT</b></p></a>
+				</li>
+				<li>
+					<a href="/makeit/"><p class="main_bottom_tag2"><b>이용약관</b></p></a>
+				</li>
+				<li>
+					<a href="/makeit/"><p class="main_bottom_tag3"><b>운영정책</b></p></a>
+				</li>
+				<li>
+					<a href="/makeit/"><p class="main_bottom_tag4"><b>개인정보취급방침</b></p></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
